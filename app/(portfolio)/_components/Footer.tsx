a35380cf3dae758ed4653bcc1a72dba857bea0be
Ray Goh <rayigo98@gmail.com>
2026-06-24 13:49:55 +0800

    fix: Element shrink on mobile device, not on PC mobile view
    project: portfolio
    
    changes:
    
    - Add height to let mobile brower respect it

--- app/(portfolio)/_components/Footer.tsx
+++ app/(portfolio)/_components/Footer.tsx
@@ -8,7 +8,7 @@ import WhatappIcon from "../_assets/icons/whatsApp.svg";
 
 const Footer = () => {
   return (
-    <section className="flex flex-col lg:flex-row lg:items-center gap-4 w-full pt-6 pb-30 md:pb-36 md:pt-8 px-0 md:px-8">
+    <section className="flex flex-col xl:flex-row xl:items-center gap-4 w-full pt-6 pb-30 md:pb-36 md:pt-8 px-0 md:px-8">
       <div className="flex items-center gap-4">
         <span className="text-lg md:text-[clamp(26px,3.3vw,46px)] font-medium tracking-tight">
           Get in touch:
@@ -18,7 +18,7 @@ const Footer = () => {
           href={"https://www.linkedin.com/in/ray-goh-chen-yi-76392b218/"}
           target="_blank"
           rel="noopener noreferrer"
-          className="shrink-0  flex items-center justify-center w-[clamp(56px,3.5vw,104px)] aspect-square p-3 md:p-[1vw] bg-white rounded-full"
+          className="shrink-0  flex items-center justify-center w-[clamp(56px,3.5vw,104px)] h-[clamp(56px,3.5vw,104px)] aspect-square p-3 md:p-[1vw] bg-white rounded-full"
           style={{
             boxShadow: "0px 0px 20px -10px #000000",
           }}
@@ -30,7 +30,7 @@ const Footer = () => {
           href={"https://github.com/rUwminu"}
           target="_blank"
           rel="noopener noreferrer"
-          className="shrink-0  flex items-center justify-center w-[clamp(56px,3.5vw,104px)] aspect-square p-3 md:p-[1vw] bg-white rounded-full"
+          className="shrink-0  flex items-center justify-center w-[clamp(56px,3.5vw,104px)] h-[clamp(56px,3.5vw,104px)] aspect-square p-3 md:p-[1vw] bg-white rounded-full"
           style={{
             boxShadow: "0px 0px 20px -10px #000000",
           }}
@@ -39,11 +39,11 @@ const Footer = () => {
         </Link>
       </div>
 
-      <div className="flex items-center gap-4">
+      <div className="flex flex-col md:flex-row md:items-center gap-4">
         <a href="mailto:your@email.com">
           <div className="flex items-center gap-4 cursor-pointer">
             <div
-              className="shrink-0 flex items-center justify-center w-[clamp(56px,3.5vw,104px)] aspect-square p-3 md:p-[1vw] bg-white rounded-full"
+              className="shrink-0 flex items-center justify-center w-[clamp(56px,3.5vw,104px)] h-[clamp(56px,3.5vw,104px)] aspect-square p-3 md:p-[1vw] bg-white rounded-full"
               style={{
                 boxShadow: "0px 0px 20px -10px #000000",
               }}
@@ -59,7 +59,7 @@ const Footer = () => {
 
         <div className="flex items-center gap-4">
           <div
-            className="shrink-0 flex items-center justify-center w-[clamp(56px,3.5vw,104px)] aspect-square p-3 md:p-[1vw] bg-white rounded-full"
+            className="shrink-0 flex items-center justify-center w-[clamp(56px,3.5vw,104px)] h-[clamp(56px,3.5vw,104px)] aspect-square p-3 md:p-[1vw] bg-white rounded-full"
             style={{
               boxShadow: "0px 0px 20px -10px #000000",
             }}
